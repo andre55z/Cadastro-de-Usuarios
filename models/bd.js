a0960ec119e2bac0613811f28a566ce1f0c92861
andredev <andluccag@gmail.com>
2025-07-01 14:27:36 -0300

    Commit de correção da segurança

--- models/bd.js
+++ models/bd.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+console.log('senha:'+process.env.DB_PASS)
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('railway', 'root', 'OPgAyTkZqwEVCIJZrsednHRCjQmnIGaH', {
+const sequelize = new Sequelize('railway', 'root', process.env.DB_PASS, {
     host: 'switchyard.proxy.rlwy.net',
     port: 47027,
     dialect: 'mysql',
